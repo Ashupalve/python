7095d92f04048da6aa4c750a9316be0723b73a25
Ashupalve <ashwedp@gmail.com>
2026-07-23 13:15:26 +0530

    new task

--- CH3_ps/QN2.py
+++ CH3_ps/QN2.py
@@ -1,1 +1,15 @@
-#write a program to fill in 
+#write a program to fill in a letter template given blow with name and date 
+'''letter = dear <name > 
+                    you are selected 
+                    <date>'''
+name = input("Enter your name for checking list : ")
+selected_studants=["Ashwed" ,"Umesh" ,"Rushikesh" ,"Atharva", "Shubham"]
+letter = '''Dear <|Name|>,
+            your are selected! 
+            <|Date|>'''
+if name in selected_studants:
+    # print ("hii")
+    print (letter .replace("<|Name|>", name ).replace("<|Date|>", "23 July 2026"))
+
+else :
+    print ("Sorry but ,Your are not selected , Try next time ")
